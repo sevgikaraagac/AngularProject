e7189e3d4981f4b28c31ebf74639071fb82fb749
sevgikaraagac <sevgikaraagac8@gmail.com>
2022-07-31 00:14:37 +0300

    29.07.2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './pages/add-product/add-product.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'homepage', pathMatch:'full'},
   {path:'homepage', component: HomepageComponent, pathMatch:'full'},
-  {path:'add-product', component: AddProductComponent, pathMatch:'full'}
+  {path:'add-product', component: AddProductComponent, pathMatch:'full'},
+  {path:'register', component: RegisterComponent}
 ];
 
 @NgModule({

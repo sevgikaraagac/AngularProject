4e66ec928d29033f9fdb5f75f65ab9f863d05495
sevgikaraagac <sevgikaraagac8@gmail.com>
2022-08-20 22:16:24 +0300

    etiya-telco-project

---
+++ etiya-telco-project/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
+import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+
+
+
+@NgModule({
+  declarations: [
+    OverlayTitleComponent,
+    SideFilterComponent,
+    UserInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    OverlayTitleComponent,
+    SideFilterComponent,
+    UserInfoComponent    
+  ]
+})
+export class SharedModule { }

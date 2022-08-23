f887a54c2ae68ba5e26403f258b25f44f3bcf9bc
sevgikaraagac <sevgikaraagac8@gmail.com>
2022-08-23 13:24:51 +0300

    contactMedium and address ngrx added

--- etiya-telco-project/src/app/features/customers/models/address.ts
+++ etiya-telco-project/src/app/features/customers/models/address.ts
@@ -1,8 +1,8 @@
 import { City } from "./city";
 
 export interface Address {
-    city: City;
-    street: string;
-    flatNumber: number;
-    description: string;
+    city?: City;
+    street?: string;
+    flatNumber?: number;
+    description?: string;
 }
